dea3b25260090985b04958508f6fe1785fdd6dec
farhaan-sh <shf.cse@gmail.com>
2023-09-29 17:11:51 +0600

    Phase 1 Initiated v1.0.1

---
+++ main.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://en.wikipedia.org/wiki/List_of_largest_companies_in_the_United_States_by_revenue'
+
+page = requests.get(url)
+
+soup = BeautifulSoup(page.text, 'html')
+
+print(soup)
+
